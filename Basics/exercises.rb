3756697345665064ef2afdcf8a9b3f08b490b179
Alexandria Collins <accollins616@gmail.com>
2019-08-28 15:49:11 -0500

    This is the first file for my Launch School repository.

---
+++ Basics/exercises.rb
@@ -0,0 +1,42 @@
+#Exercise 1
+"Alexandria " + "Collins"
+
+#Exercise 2
+num = 1234
+
+thousands = num / 1000
+hundreds = num % 1000 / 100
+tens = num % 1000 % 200 / 10
+ones = num % 1000 % 200 % 10
+
+#Exercise 3
+movies = {
+  avatar: 2009,
+  star_wars: 1977,
+  aliens: 1986
+}
+
+movies.each {|k,v| puts v}
+
+#Exercise 4
+release_dates = [2009, 1977, 1986]
+release_dates.each {|x| puts x}
+
+#Exercise 5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#Exercise 6
+puts 3.0 * 3.0
+puts 4.5 * 4.5
+puts 6.7 * 6.7
+
+#Exercise 7
+#This error tells me that the user used a parenthesis rather than a bracket. 
+
+
+
+
+
