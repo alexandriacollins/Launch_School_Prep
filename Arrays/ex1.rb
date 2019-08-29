c488309ef514650b2aa5e20ca5bc03e35a561141
Alexandria Collins <alexandercollins2016@u.northwestern.edu>
2019-08-29 12:45:14 -0500

    These are my Arrays exercises.

---
+++ Arrays/ex1.rb
@@ -0,0 +1,8 @@
+# exercise_1.rb
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.include?(number)
+  puts "#{number} is in this array."
+end
---
+++ Arrays/ex7.rb
@@ -0,0 +1,8 @@
+# exercise_7.rb
+
+arr = [1, 2, 3, 4, 5]
+
+arr_mod = arr.map {|x| x + 2}
+
+p arr
+p arr_mod
